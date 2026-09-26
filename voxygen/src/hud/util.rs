c1b949bc30fdcd36582c994da2f5cefa86a70c1d
Cesar Augusto Benitez <kambire2007@gmail.com>
2026-09-26 02:41:06 -0300

    feat(tamer): replace channel beam with dynamic Power Orb projectile ability (v0.18.16)

--- voxygen/src/hud/util.rs
+++ voxygen/src/hud/util.rs
@@ -720,7 +720,7 @@ pub fn ability_image(imgs: &img_ids::Imgs, ability_id: &str) -> image::Id {
         "common.abilities.sceptre.sanacion" => imgs.skill_sceptre_heal,
         "common.abilities.sceptre.wardingaura" => imgs.skill_sceptre_aura,
         // Entrenador
-        "common.abilities.entrenador.latigo_espinas" => imgs.skill_sceptre_lifesteal,
+        "common.abilities.entrenador.latigo_espinas" => imgs.fireball,
         "common.abilities.entrenador.aura_vital" => imgs.skill_sceptre_heal,
         "common.abilities.entrenador.vinculo_salvaje" => imgs.bow_ardent_hunt,
         // Shield
